eaac567b405d5a95e27f29af83760ae7c8393670
Sven-27 <sven.notermans@live.nl>
2022-08-03 18:51:39 +0200

    added links with a submenu

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
 const menuIcon = document.getElementById('menu-icon');
 const closeIcon = document.getElementById('close-icon'); 
 const nav = document.querySelector('nav');	
+const dropdown = document.querySelector('#dropdown');
+const button = document.querySelector('.dd-btn');
 
 const toggleMenu = () => {
   nav.classList.toggle('menu');
 }
 
+const showDropdown = () => {
+  dropdown.classList.toggle('show');
+}
+
 menuIcon.addEventListener('click', toggleMenu);
 closeIcon.addEventListener('click', toggleMenu);
+button.addEventListener('click', showDropdown);
